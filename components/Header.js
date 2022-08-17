400ae7692b115fb7d16b463848ae934a779e6b9f
Brandon Rainey <brandonhrainey@gmail.com>
2022-08-17 01:36:56 -0400

    added filtered follow map of data

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,23 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 export default function Header() {
-
-  
-
-
-  
-
-
-  
-  
-
-  
-
-
-
-
+  const router = useRouter()
 
 
   return (
-    <div>Header</div>
+    <div className="flex items-center">
+      <p className="text-4xl font-bold pl-4 pt-4">Twitch Follow Tracker</p>
+      <div
+        className="bg-purple-500 w-36 p-1 rounded-2xl h-8 text-white font-bold text-center ml-auto mr-4"
+        onClick={() =>
+          router.push(
+            'https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=mz3oo6erk0hqgzs6o8ydh26c9m8u09&redirect_uri=http://localhost:3000&scope=user%3Aread%3Afollows&state=c3ab8aa609ea11e793ae92361f002671'
+          )
+        }
+      >
+        {'Connect to Twitch'}
+      </div>
+    </div>
   )
 }
--- components/Main.js
+++ components/Main.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-export default function Main() {
+export default function Main({ followerData }) {
+    console.log(followerData)
   return (
-    <div>
-        
+    <div className='flex flex-wrap gap-1'>
+        {followerData.filter(item => item).map((item) => (
+        <div className='flex'>
+            <img src={`${item.profile_image_url}`}/>
+          <p>{item.login}</p>
+        </div>
+      )
+
+      )}
     </div>
   )
 }
